59448a0736abfdcf6ba3dfbdb45fb714fe648c92
lokesh <g.sailokesh9@gmail.com>
2025-09-20 15:00:15 +0530

    Modified subscriber_altimeter.cpp file to use shm manager

--- nova_interface/src/subscriber_altimeter.cpp
+++ nova_interface/src/subscriber_altimeter.cpp
@@ -12,57 +12,60 @@
 class SubscriberAltimeter : public rclcpp::Node {
 private:
     rclcpp::Subscription<ros_gz_interfaces::msg::Altimeter>::SharedPtr subscription_;
-    nv_shm_obj_t shm;
-    nv_shm_data shm_data;
+    nv_shm_metadata_t metadata;
+    nv_shm_mgr_header header;
     nv_altimeter_data altimeter_data;
+    int loop;
+    int count;
+    int shm_fd;
 public:
     SubscriberAltimeter() : Node("node_subscriber_altimeter"){
-        shm.shm_id = SHM_SENSOR_ALTIMETER;
-        shm.shm_size = sizeof(nv_shm_data_t) + sizeof(nv_altimeter_data_t);
-        snprintf(shm.shm_name, sizeof(shm.shm_name), "/shm_id_%d", shm.shm_id);
-        int shm_fd = shm_open(shm.shm_name, O_CREAT | O_RDWR, 0660);
-        if(shm_fd == -1) perror("shm_open failed");
+        count = 10;
+        snprintf(metadata.shm_name, SHM_NAME_MAX, "altimeter_shm\n");
+        snprintf(metadata.pname, PROCESS_NAME_MAX, "altimeter_interface\n");
 
-        if (ftruncate(shm_fd, shm.shm_size) == -1) perror("ftruncate");
+        shm_fd = nvShmManager::nvMapShm(&header);
 
-        shm.data = (nv_shm_data)mmap(0, shm.shm_size, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-        if(shm.data == MAP_FAILED) perror("mmap failed");
+        nvShmManager::nvGetShmPtr(header, sizeof(nv_altimeter_data_t), count, &metadata);
 
-        shm.data->data_ptr = shm.data + sizeof(nv_shm_data_t);
-        shm_data = (nv_shm_data)shm.data;
-        altimeter_data = (nv_altimeter_data)shm_data->data_ptr;
-        printf("shm init done \n");
+        altimeter_data = (nv_altimeter_data)metadata.data->data_ptr;
+
+        loop = 0;
 
 #ifndef USE_SEMAPHORE
-        pthread_mutexattr_init(&shm.attr);
-        pthread_mutexattr_setpshared(&shm.attr, PTHREAD_PROCESS_SHARED);
-        pthread_mutex_init(&shm.lock, &shm.attr);
-        pthread_cond_init(&shm.cond);
-        shm.flag = 0;
+        pthread_mutexattr_init(&metadata.data->attr);
+        pthread_mutexattr_setpshared(&metadata.data->attr, PTHREAD_PROCESS_SHARED);
+        pthread_mutex_init(&metadata.data->lock, &metadata.data->attr);
+        pthread_cond_init(&metadata.data->cond);
+        metadata.data->flag = 0;
 #else
-        printf("before sem init \n");
-        sem_init(&shm_data->write_sem, 1, 1);
-        printf("init write sem done \n");
-        sem_init(&shm_data->read_sem, 1, 0);
+        sem_init(&metadata.data->read_sem, 1, 0);
+        sem_init(&metadata.data->write_sem, 1, count);
 #endif
 
         auto topic_callback = [this](ros_gz_interfaces::msg::Altimeter::UniquePtr msg) -> void {
 #ifndef USE_SEMAPHORE
-            pthread_mutex_lock(&shm.lock);
+            pthread_mutex_lock(&metadata.data->lock);
 #else
-            sem_wait(&shm_data->write_sem);
+            sem_wait(&metadata.data->write_sem);
 #endif
             altimeter_data->vertical_position = msg->vertical_position;
             altimeter_data->vertical_reference = msg->vertical_reference;
             altimeter_data->vertical_velocity = msg->vertical_velocity;
 
 #ifndef USE_SEMAPHORE
-            pthread_mutex_unlock(&shm.lock);
+            pthread_mutex_unlock(&metadata.data->lock);
 #else
-            sem_post(&shm_data->read_sem);
+            sem_post(&metadata.data->read_sem);
 #endif
+            loop++;
         };
         subscription_ = this->create_subscription<ros_gz_interfaces::msg::Altimeter>("/nova/altimeter", 10, topic_callback);
+    }
+
+    ~SubscriberAltimeter(){
+        munmap(altimeter_data, sizeof(nv_altimeter_data_t));
+        close(shm_fd);
     }
 };
 
